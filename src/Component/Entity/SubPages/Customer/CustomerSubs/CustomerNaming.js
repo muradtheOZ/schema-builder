49d9e453a3530a645d729f5d8f62b002f32f9ca8
Murad Ahmed <murad15-9149@diu.edu.bd>
2021-06-01 22:31:37 +0600

    table static

--- src/Component/Entity/SubPages/Customer/CustomerSubs/CustomerNaming.js
+++ src/Component/Entity/SubPages/Customer/CustomerSubs/CustomerNaming.js
@@ -1,50 +1,38 @@
 import React, { useMemo } from "react";
-import { useTable } from "react-table";
-import { COLUMNS } from "../../../../Columns";
-import mock_data from "../../../../mock_data.json";
+import { Table } from "react-bootstrap";
 
 const CustomerNaming = () => {
-  const colmuns = useMemo(() => COLUMNS, []);
 
-  const data = useMemo(() => mock_data, []);
-
-  const tableInstance = useTable({
-    colmuns,
-    data,
-  });
-
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
   return (
-    <div>
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}> {column.render("Header")}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                  {
-                      row.cells.map((cell)=>{
-                        <td  {...cell.getCellProps()}> {cell.render("Cell")}</td>
-
-                      })}
-                
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>
   );
 };
 
